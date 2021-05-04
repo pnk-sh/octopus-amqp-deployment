549ed9bf10c43068b1b71e3bfa88d73fca002b89
Paris Nakita Kejser <contact@parisnakitakejser.com>
2021-05-04 16:54:23 +0200

    add first beta commit to give huge up in cluster queue

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import logger from "./models/logger";
 
 require('dotenv').config()
 
-amqp.connect(`amqp://${process.env.AMQP_HOST}`, function(error0: any, connection: any) {
+amqp.connect(`${process.env.AMQP_HOST}`, function(error0: any, connection: any) {
   if (error0) {
     throw error0;
   }
@@ -14,30 +14,35 @@ amqp.connect(`amqp://${process.env.AMQP_HOST}`, function(error0: any, connection
     if (error1) {
       throw error1;
     }
+    
+    axios.get(`${process.env.REST_API_HOST}info`).then((resp) => {
+      const cluster_id = resp.data.resualt.Swarm.Cluster.ID;
+      
+      const queue = `${process.env.AMQP_QUEUE}-${cluster_id}`;
 
-    const queue = process.env.AMQP_QUEUE;
+      channel.assertQueue(queue, {
+        durable: false
+      });
 
-    channel.assertQueue(queue, {
-      durable: false
-    });
+      logger.info(" [*] Waiting for messages in %s.", queue);
 
-    logger.info(" [*] Waiting for messages in %s.", queue);
+      channel.consume(queue, (msg: any) => {
+        const json_data = JSON.parse(msg.content.toString());
 
-    channel.consume(queue, (msg: any) => {
-      const json_data = JSON.parse(msg.content.toString());
+        logger.info(" [x] Received %s", json_data);
 
-      logger.info(" [x] Received %s", json_data);
-
-      axios.post(`${process.env.REST_API_HOST}service/${json_data.service_id}/update`, {
-        image: json_data.image,
-        tag: json_data.tag,
-      }).then((resp) => {
-        logger.info(resp)
-      }).catch((err) => {
-        logger.error(err)
-      })
-    }, {
-      noAck: true
-    });
+        axios.post(`${process.env.REST_API_HOST}service/${json_data.service_id}/update`, {
+          image: json_data.image,
+          tag: json_data.tag,
+        }).then((resp) => {
+          logger.info(resp)
+        }).catch((err) => {
+          logger.error(err)
+        })
+      }, {
+        noAck: true
+      });
+    })
+    
   });
 });
